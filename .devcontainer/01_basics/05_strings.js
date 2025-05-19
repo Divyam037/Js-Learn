453ff441429f87cd12e6f62d965947e392d64285
Divyam Bhushan <divyambhushan37@gmail.com>
2025-05-19 13:30:18 +0000

    basic

--- .devcontainer/01_basics/05_strings.js
+++ .devcontainer/01_basics/05_strings.js
@@ -5,16 +5,16 @@ const repoCount = 50
 
 //console.log(`Hello my name is ${name} and my repo count is ${repoCount}`); 
 
-const gameName = new String('hitesh-hc-com')
+const gameName = new String('divyam-hc-com')
 
-// console.log(gameName[0]);
-// console.log(gameName.__proto__);
+//  console.log(gameName[0]);
+//  console.log(gameName.__proto__);
 
 
 // console.log(gameName.length);
 // console.log(gameName.toUpperCase());
-console.log(gameName.charAt(2));
-console.log(gameName.indexOf('t'));
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
 
 const newString = gameName.substring(0, 4)
 console.log(newString);
@@ -22,14 +22,14 @@ console.log(newString);
 const anotherString = gameName.slice(-8, 4)
 console.log(anotherString);
 
-const newStringOne = "   hitesh    "
+const newStringOne = "   divyam    "
 console.log(newStringOne);
-console.log(newStringOne.trim());
+console.log(newStringOne.trim()); //remove extra space
 
 const url = "https://hitesh.com/hitesh%20choudhary"
 
 console.log(url.replace('%20', '-'))
 
-console.log(url.includes('sundar'))
+console.log(url.includes('com')) //boolean
 
-console.log(gameName.split('-'));
+console.log(gameName.split('-')); //deconstruct in array
